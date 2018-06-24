927c04a9ae8f823ff9ea45e8365d2fd4bfb9f9b0
kiran dalai <kirandalai@kirans-MacBook-Air.local>
2018-06-24 22:01:48 +0530

    added maven

--- src/test/java/loginpage/Loginpage.java
+++ src/test/java/loginpage/Loginpage.java
@@ -2,18 +2,28 @@ package loginpage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Loginpage {
+	
+	public static WebDriver driver;
+	
+@BeforeTest
+public void login() 
+{
+	  
+	  System.setProperty("webdriver.chrome.driver", "/Users/kirandalai/Downloads/chromedriver");
+	  driver=new ChromeDriver();
+		driver.get("https://www.bigbasket.com/");
+		driver.manage().window().maximize();
+	//Application was launched
+		System.out.println("URL:"+ driver.getCurrentUrl());
+}
 	@Test
-	public void login() {
-		  
-		  System.setProperty("webdriver.chrome.driver", "/Users/kirandalai/Downloads/chromedriver");
-			WebDriver driver=new ChromeDriver();
-			driver.get("https://www.bigbasket.com/");
-			System.out.println("URL:"+ driver.getCurrentUrl());
-			driver.manage().window().maximize();
-		//Application was launched
-			
+	public void loginapp()
+	{
+	
+	System.out.println("login sucess");
 	}
 }
